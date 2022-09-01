593bd8ca76e6b9723295afa922ad66bc81128761
A-Tirta <tirtananda68@gmail.com>
2022-09-01 20:05:33 +0700

    Update models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
+from email.message import Message
 from django.db import models
 
 # Create your models here.
+
+class BookTable(models.Model):
+  Name = models.CharField(max_length=255)
+  Email = models.EmailField(max_length=255)
+  Phone = models.CharField(max_length=13)
+  Date = models.DateField()
+  Time = models.CharField(max_length=50)
+  ManyPeople = models.CharField(max_length=100)
+  Message = models.TextField()
