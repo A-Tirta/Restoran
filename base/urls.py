45d1d0c846b6e0b036e3584daa587a362fb624cf
A-Tirta <tirtananda68@gmail.com>
2022-09-01 20:05:49 +0700

    Update urls

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="Landing page"),
-    path('menu/', views.menu, name="menu page"),
-    path('login/', views.login, name="login page")
 ]
--- base/views.py
+++ base/views.py
@@ -1,13 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 # Create your views here.
 
 def index(request):
   return render(request, 'index.html')
-
-def menu(request):
-  return render(request, 'menu.html')
-
-def login(request):
-  return render(request, 'login.html')
