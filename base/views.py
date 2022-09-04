274b67300ee3a6d34b047299694a259bfbd80d1e
A-Tirta <tirtananda68@gmail.com>
2022-09-04 18:34:43 +0700

    Update views

--- base/views.py
+++ base/views.py
@@ -1,19 +1,21 @@
 from multiprocessing import context
 from urllib.request import Request
 from django.shortcuts import render
-from .models import BookTable
-from .forms import BookTableForm
+from .models import BookTable, ContactUs
+from .forms import BookTableForm, ContactUsForm
 
 # Create your views here.
 
 def index(request):
   postBookTable = BookTableForm()
+  postContactUs = ContactUsForm()
 
   if request.method == 'POST':
     print(request.POST)
 
   context = {
-    'postBookTable' : postBookTable
+    'postBookTable' : postBookTable,
+    'postContactUs' : postContactUs
   }
 
   return render(request, 'index.html', context)
