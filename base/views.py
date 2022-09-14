b70e8db84c0ae0ad7e689d151e90c070f349172a
A-Tirta <tirtananda68@gmail.com>
2022-09-14 21:42:44 +0700

    Update views

--- base/views.py
+++ base/views.py
@@ -1,21 +1,35 @@
+from ast import If
+from http.client import HTTPResponse
 from multiprocessing import context
 from urllib.request import Request
 from django.shortcuts import render
 from .models import BookTable, ContactUs
 from .forms import BookTableForm, ContactUsForm
+from django.http import HttpResponseRedirect
+from django.contrib import auth
 
 # Create your views here.
 
 def index(request):
-  postBookTable = BookTableForm()
   postContactUs = ContactUsForm()
-
-  if request.method == 'POST':
-    print(request.POST)
+  postBookTable = BookTableForm()
 
   context = {
     'postBookTable' : postBookTable,
     'postContactUs' : postContactUs
   }
+
+  if request.method == 'POST':
+    BookTable.objects.create(
+    Name = request.POST["Name"],
+    Email = request.POST["Email"],
+    Phone = request.POST["Phone"],
+    Date = request.POST["Date"],
+    Time = request.POST["Time"],
+    ManyPeople = request.POST["ManyPeople"],
+    Message = request.POST["Message"]
+  )
+
+    return HttpResponseRedirect("/home/")
 
   return render(request, 'index.html', context)
